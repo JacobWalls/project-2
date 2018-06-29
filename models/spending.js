e6af1630ca5b01f356e43a797e4b83375eff4c7b
lydiarachel <lydiarachel@gmail.com>
2018-06-29 19:53:56 -0400

    adding the models spending and user

--- models/spending.js
+++ models/spending.js
@@ -1,7 +1,48 @@
-module.exports = function (sequelize, DataTypes) {
-    var Todo = sequelize.define("Todo", {
-        text: DataTypes.STRING,
-        complete: DataTypes.BOOLEAN
+module.exports = function(sequelize, DataTypes) {
+    var Spending = sequelize.define("Spending", {
+      housing: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      utilities: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      phone: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      cable_internet: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      food: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      clothing: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      beauty: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      entertainment: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
     });
-    return Todo;
-};
+  
+    Spending.associate = function(models) {
+      // We're saying that a Spending should belong to a User
+      // Spending can't be created without a user due to the foreign key constraint
+      Spending.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Spending;
+  };
