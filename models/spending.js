b7bea79338713af903228e78addd8fd39293db0f
lydiarachel <lydiarachel@gmail.com>
2018-06-30 13:19:33 -0400

    updates to the spending.js models with monthly income and isNumeric and len

--- models/spending.js
+++ models/spending.js
@@ -1,40 +1,59 @@
 module.exports = function(sequelize, DataTypes) {
     var Spending = sequelize.define("Spending", {
+
       monthlyIncome: {
         type: DataTypes.INTEGER,
         allowNull: true,
+        isNumeric: true,
+        len: [0, 1000000]
       },
       housing: {
         type: DataTypes.INTEGER,
         allowNull: true,
+        isNumeric: true,
+        len: [0, 1000000]
       },
       utilities: {
         type: DataTypes.INTEGER,
         allowNull: true,
+        isNumeric: true,
+        len: [0, 1000000]
       },
       phone: {
         type: DataTypes.INTEGER,
         allowNull: true,
+        isNumeric: true,
+        len: [0, 1000000]
       },
       cable_internet: {
         type: DataTypes.INTEGER,
         allowNull: true,
+        isNumeric: true,
+        len: [0, 1000000]
       },
       food: {
         type: DataTypes.INTEGER,
         allowNull: true,
+        isNumeric: true,
+        len: [0, 1000000]
       },
       clothing: {
         type: DataTypes.INTEGER,
         allowNull: true,
+        isNumeric: true,
+        len: [0, 1000000]
       },
       beauty: {
         type: DataTypes.INTEGER,
         allowNull: true,
+        isNumeric: true,
+        len: [0, 1000000]
       },
       entertainment: {
         type: DataTypes.INTEGER,
         allowNull: true,
+        isNumeric: true,
+        len: [0, 1000000]
       },
     });
   
@@ -50,65 +69,4 @@ module.exports = function(sequelize, DataTypes) {
   
     return Spending;
   };
-    //     monthlyIncome: {
-    //         type: DataTypes.INTEGER,
-    //         isNumeric: true,
-    //         len: [0, 100000]
-    //     },
-    //     housing: {
-    //         type: DataTypes.INTEGER,
-    //         isNumeric: true,
-    //         len: [0, 100000]
-    //     },
-    //     utilities: {
-    //         type: DataTypes.INTEGER,
-    //         isNumeric: true,
-    //         len: [0, 100000]
-    //     },
-    //     phone: {
-    //         type: DataTypes.INTEGER,
-    //         isNumeric: true,
-    //         len: [0, 100000]
-    //     },
-    //     cable_internet: {
-    //         type: DataTypes.INTEGER,
-    //         isNumeric: true,
-    //         len: [0, 100000]
-    //     },
-    //     food: {
-    //         type: DataTypes.INTEGER,
-    //         isNumeric: true,
-    //         len: [0, 100000]
-    //     },
-    //     clothing: {
-    //         type: DataTypes.INTEGER,
-    //         isNumeric: true,
-    //         len: [0, 100000]
-    //     },
-    //     beauty: {
-    //         type: DataTypes.INTEGER,
-    //         isNumeric: true,
-    //         len: [0, 100000]
-    //     },
-    //     entertainment: {
-    //         type: DataTypes.INTEGER,
-    //         isNumeric: true,
-    //         len: [0, 100000]
-    //     }
-
-    // });
-
-//     Spending.associate = function (models) {
-//         //a spending budget must belong to a user_name
-//         //a spending budget can't be created without a user_name
-//         Spending.belongsTo(models.User, {
-//             foreignKey: {
-//                 allowNull: false
-//             }
-//         });
-//     };
-//     return Spending;
-// };
-
-/*If time permits, we can come back to add more validations.  For now, I am
-only validating for an integer */
+ 
