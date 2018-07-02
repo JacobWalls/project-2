c346c18963567b565db080a6473fe0c5a3e681ca
KentonDunn <kenton.dunn@gmail.com>
2018-07-02 17:04:41 -0400

    a few changes

--- models/spending.js
+++ models/spending.js
@@ -1,72 +1,71 @@
-module.exports = function(sequelize, DataTypes) {
-    var Spending = sequelize.define("Spending", {
+module.exports = function (sequelize, DataTypes) {
+  var Spending = sequelize.define("Spending", {
 
-      monthlyIncome: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        isNumeric: true,
-        len: [0, 1000000]
-      },
-      housing: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        isNumeric: true,
-        len: [0, 1000000]
-      },
-      utilities: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        isNumeric: true,
-        len: [0, 1000000]
-      },
-      phone: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        isNumeric: true,
-        len: [0, 1000000]
-      },
-      cable_internet: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        isNumeric: true,
-        len: [0, 1000000]
-      },
-      food: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        isNumeric: true,
-        len: [0, 1000000]
-      },
-      clothing: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        isNumeric: true,
-        len: [0, 1000000]
-      },
-      beauty: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        isNumeric: true,
-        len: [0, 1000000]
-      },
-      entertainment: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        isNumeric: true,
-        len: [0, 1000000]
-      },
+    monthlyIncome: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isNumeric: true,
+      len: [0, 1000000]
+    },
+    housing: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isNumeric: true,
+      len: [0, 1000000]
+    },
+    utilities: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isNumeric: true,
+      len: [0, 1000000]
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isNumeric: true,
+      len: [0, 1000000]
+    },
+    cable_internet: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isNumeric: true,
+      len: [0, 1000000]
+    },
+    food: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isNumeric: true,
+      len: [0, 1000000]
+    },
+    clothing: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isNumeric: true,
+      len: [0, 1000000]
+    },
+    beauty: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isNumeric: true,
+      len: [0, 1000000]
+    },
+    entertainment: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isNumeric: true,
+      len: [0, 1000000]
+    },
+  });
+
+  Spending.associate = function (models) {
+    // We're saying that a Spending should belong to a User
+    // Spending can't be created without a user due to the foreign key constraint
+    Spending.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
     });
-  
-    Spending.associate = function(models) {
-      // We're saying that a Spending should belong to a User
-      // Spending can't be created without a user due to the foreign key constraint
-      Spending.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-  
-    return Spending;
   };
- 
+
+  return Spending;
+};
--- public/js/budget.js
+++ public/js/budget.js
@@ -1,27 +1,28 @@
-var spendingChart = document.getElementById("spendingChart");
+// var spendingChart = document.getElementById("spendingChart");
 
-Chart.defaults.global.defaultFontFamily = "Lato";
-Chart.defaults.global.defaultFontSize = 18;
+// Chart.defaults.global.defaultFontFamily = "Lato";
+// Chart.defaults.global.defaultFontSize = 18;
 
-var spendingData = {
-  label: 'Spending',
-  data: [5427, 5243, 5514, 3933, 1326, 687, 1271, 1638]
-};
+// var spendingData = {
+//   label: 'Spending',
+//   data: [5427, 5243, 5514, 3933, 1326, 687, 1271, 1638]
+// };
 
-var barChart = new Chart(spendingChart, {
-  type: 'bar',
-  data: {
-    labels: ["Monthly Income", "Housing", "Utilities", "Phone", "Cable/Internet", "Food", "Clothing", "Beauty", "Entertainment"],
-    datasets: [spendingData]
-  }
-});
+// var barChart = new Chart(spendingChart, {
+//   type: 'bar',
+//   data: {
+//     labels: ["Monthly Income", "Housing", "Utilities", "Phone", "Cable/Internet", "Food", "Clothing", "Beauty", "Entertainment"],
+//     datasets: [spendingData]
+//   }
+// });
 
 // adding button functionality to our budget page
 
-$("#pushButton").on("click", function(event) {
+$("#pushButton").on("click", function (event) {
   event.preventDefault();
 
   var userData = {
+
     monthlyIncome: $("#monthlyIncome").val().trim(),
     housing: $("#housing").val().trim(),
     utilities: $("#utilities").val().trim(),
@@ -33,11 +34,11 @@ $("#pushButton").on("click", function(event) {
     entertainment: $("#entertainment").val().trim(),
   };
 
-  $.post("/api/...", userData)
+  $.post("/newbudget", userData)
 
-  .then(function(data) {
-    console.log(data);
-  })
+    .then(function (data) {
+      console.log("this came back" + data);
+    })
 
   $("#monthlyIncome").val("");
   $("#housing").val("");
@@ -49,4 +50,3 @@ $("#pushButton").on("click", function(event) {
   $("#beauty").val("");
   $("#entertainment").val("");
 })
-
