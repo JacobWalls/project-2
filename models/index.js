37d4a4a077b19fe0213d2b77b61c1bbef791559c
KentonDunn <kenton.dunn@gmail.com>
2018-07-02 21:40:53 -0400

    mysql and jawsdb updates

--- models/index.js
+++ models/index.js
@@ -4,11 +4,13 @@ var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
 var basename = path.basename(__filename);
-var env = process.env.NODE_ENV || 'test';
+var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../config/config.json')[env];
+
 var db = {};
 
 if (config.use_env_variable) {
+  
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
